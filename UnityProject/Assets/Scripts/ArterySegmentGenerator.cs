51e6053b10112429e2b2ee15945ad0a6946aabac
Ana Djianto <trashythingsandstuff@gmail.com>
2025-01-31 00:59:46 -0500

    Add Camera Movement

--- UnityProject/Assets/Scripts/ArterySegmentGenerator.cs
+++ UnityProject/Assets/Scripts/ArterySegmentGenerator.cs
@@ -7,16 +7,23 @@ public class ArterySegmentGenerator : MonoBehaviour
     public float segmentShiftLength;
     public float angleScaling;
     public float generateCooldown;
+    public int initialTunnelLength = 5; 
+    public CameraMovement cameraMovement;
 
     private Vector3 currPos;
     private Vector3 prevPos;
     private Vector3 currDirection;
+    private List<Vector3> points;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        points = new List<Vector3>();
         currPos = transform.position;
         currDirection = new Vector3(0, 0, segmentShiftLength);
+        for(int i = 0; i < initialTunnelLength; i++)
+            GenerateNewSegment();
+
         InvokeRepeating("GenerateNewSegment", generateCooldown, generateCooldown);
     }
 
@@ -27,7 +34,13 @@ public class ArterySegmentGenerator : MonoBehaviour
         Quaternion newSegmentRotate = Quaternion.LookRotation(currDirection) * Quaternion.Euler(90,0,0);
         GameObject newSegment = (GameObject)Instantiate(segmentPrefab, newSegmentPos, newSegmentRotate);
 
+        UpdateCameraPath();
         GenerateNewDirection();
+    }
+
+    void UpdateCameraPath() {
+        points.Add(currPos);
+        cameraMovement.SetPoints(points);
     }
 
     void GenerateNewDirection() {
