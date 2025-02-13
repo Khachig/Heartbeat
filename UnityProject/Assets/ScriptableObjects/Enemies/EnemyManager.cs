82c623dd5dec72fecdd3b89219ce15eed9a65262
Ana Djianto <trashythingsandstuff@gmail.com>
2025-02-12 23:14:07 -0500

    make enemy follow camera

--- UnityProject/Assets/ScriptableObjects/Enemies/EnemyManager.cs
+++ UnityProject/Assets/ScriptableObjects/Enemies/EnemyManager.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyManager", menuName = "Scriptable Objects/EnemyManager")]
 public class EnemyManager : ScriptableObject
 {
+    public float spawnForwardOffset = 20f;
     [SerializeField] private GameObject enemyPrefab;
 
     public struct SpawnParameters
     {
         public Vector3 position;
         public Quaternion rotation;
+        public Camera camera;
     }
 
     public void init()
@@ -18,12 +20,13 @@ public class EnemyManager : ScriptableObject
 
     public void spawnEnemy(SpawnParameters parameters)
     {
-        GameObject enemy = Instantiate(enemyPrefab, parameters.position, Quaternion.identity);
+        GameObject enemy = Instantiate(enemyPrefab, parameters.position + parameters.camera.transform.forward * spawnForwardOffset, Quaternion.identity);
         enemy.transform.eulerAngles = new Vector3(
             parameters.rotation.x,
             parameters.rotation.y,
             parameters.rotation.z
         );
+        enemy.transform.parent = parameters.camera.transform;
         /* EnemyBehaviour script = enemy.GetComponent<EnemyBehaviour>(); */
         /* script.init(); */
     }
--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -4,17 +4,20 @@ public class GameManager : MonoBehaviour
 {
     // TODO: include PlayerManager, LevelManager, whatever is needed etc.
     [SerializeField] private EnemyManager enemyManager;
+    public Camera camera;
 
     void Start()
     {
         enemyManager.init();
-        // enemyManager.spawnEnemy(new EnemyManager.SpawnParameters {
-        //     position = new Vector3(2, -1, 5),
-        //     rotation = new Quaternion(0, 180, 0, 1),
-        // });
-        // enemyManager.spawnEnemy(new EnemyManager.SpawnParameters {
-        //     position = new Vector3(-2, -1, 3),
-        //     rotation = new Quaternion(0, 180, 0, 1),
-        // });
+        enemyManager.spawnEnemy(new EnemyManager.SpawnParameters {
+            position = new Vector3(2, -1, 5),
+            rotation = new Quaternion(0, 180, 0, 1),
+            camera = camera,
+        });
+        enemyManager.spawnEnemy(new EnemyManager.SpawnParameters {
+            position = new Vector3(-2, -1, 3),
+            rotation = new Quaternion(0, 180, 0, 1),
+            camera = camera,
+        });
     }
 }
