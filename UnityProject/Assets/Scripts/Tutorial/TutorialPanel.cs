187791b429d1bc61f8fa9ab0d5703ad1a6570b87
Olivia <oliviayzhou@gmail.com>
2025-02-26 00:20:08 -0500

    fix space bar bug tutorial

--- UnityProject/Assets/Scripts/Tutorial/TutorialPanel.cs
+++ UnityProject/Assets/Scripts/Tutorial/TutorialPanel.cs
@@ -18,7 +18,7 @@ public class TutorialPanelScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             HidePanel();
         }
