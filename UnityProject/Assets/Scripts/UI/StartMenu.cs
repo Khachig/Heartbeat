b542a75779f4f52737e297d5edd2bfc70035309a
Khachig Ainteblian <me@khachig.com>
2025-03-19 10:41:38 -0400

    add start menu

--- UnityProject/Assets/Scripts/UI/StartMenu.cs
+++ UnityProject/Assets/Scripts/UI/StartMenu.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class StartMenu : MonoBehaviour
 {
     public Button playButton, quitButton;
+
+    private int selected = 0;
 
     void Start()
     {
@@ -14,18 +17,44 @@ public class StartMenu : MonoBehaviour
         playButton.onClick.AddListener(OnQuitButton);
     }
 
-    /* void Update() */
-    /* { */
-    /*     if (Input.GetKeyDown(KeyCode.Return)) */
-    /*     { */
-    /*         Debug.Log($"CLICKED ${SceneManager.GetActiveScene().buildIndex}"); */
-    /*         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); */
-    /*     } */
-    /* } */
+    void Update()
+    {
+        bool enterPressed = Keyboard.current.enterKey.wasPressedThisFrame;
+        bool submitted = enterPressed;
+        if (Gamepad.current != null)
+        {
+            bool buttonPressed = Gamepad.current.aButton.wasPressedThisFrame;
+            submitted = submitted || buttonPressed;
+        }
+        if (enterPressed) {
+            if (selected == 0) OnPlayButton();
+            else OnQuitButton();
+        }
+
+        bool upPressed = Keyboard.current.upArrowKey.wasPressedThisFrame;
+        bool downPressed = Keyboard.current.downArrowKey.wasPressedThisFrame;
+
+        if (upPressed) {
+            selected = selected == 1 ? 0 : 1;
+        }
+        if (downPressed) {
+            selected = selected == 0 ? 1 : 0;
+        }
+
+        if (selected == 0) {
+            playButton.GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1.2f, 1f);
+            quitButton.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+        }
+        else {
+            playButton.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+            quitButton.GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1.2f, 1f);
+        }
+
+    }
 
     public void OnPlayButton()
     {
-        SceneManager.LoadScene("AlphaSceneNewTube");
+        SceneManager.LoadScene("DodgeAttackPhases");
     }
 
     public void OnQuitButton()
