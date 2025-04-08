ef119db81200ba8d060ebdd171e244367163e8c7
Olivia <oliviayzhou@gmail.com>
2025-04-08 19:29:54 -0400

    adjust off limit lanes, with delay damage, only on projectile phase

--- UnityProject/Assets/Scripts/Enemy/EnemyRhythmManager.cs
+++ UnityProject/Assets/Scripts/Enemy/EnemyRhythmManager.cs
@@ -35,6 +35,7 @@ public class EnemyRhythmManager : MonoBehaviour, IEasyListener
     private int difficulty = 0; // 0 = easy; 1 = normal, -1 = tutorial
     private int wave = -2;
     private int rhythmSequencesPlayed = 0;
+    private int waveStartBar = 0;
 
     private void Start()
     {
@@ -122,7 +123,7 @@ public class EnemyRhythmManager : MonoBehaviour, IEasyListener
             hasStartedCombo = true;
 
         comboNum++;
-        Effects.SpecialEffects.ComboContinueEffect(comboNum);
+        // Effects.SpecialEffects.ComboContinueEffect(comboNum);
         FMOD.Studio.EventInstance comboHitInstance = RuntimeManager.CreateInstance(ComboHit);
         comboHitInstance.setParameterByName("ComboCount", comboNum);
         comboHitInstance.start();
@@ -253,7 +254,7 @@ public class EnemyRhythmManager : MonoBehaviour, IEasyListener
         // Check if it's time to trigger an attack (next attack in 2 beats)
         else if (ShouldTriggerRhythmAttack(currentBar, currentBeat, beatsPerBar, nextAttackBeat))
         {
-            TriggerEnemyAttack();
+            TriggerEnemyAttack(currentBar, currentBeat, beatsPerBar);
         }
         // Check if the phase should transition between melee and projectile
         else if (ShouldSwitchAttackPhase(currentBar))
@@ -301,7 +302,7 @@ public class EnemyRhythmManager : MonoBehaviour, IEasyListener
     {
         return hasStartedRhythmSequence &&
             (isProjectilePhase || (IsBossWave() && currBossWave < numBossWaves)) &&
-            currentBar > lastSequencePlayedBar + 1;
+            currentBar > lastSequencePlayedBar + 1; //&& !isProjectileTutorial(currentBar)
     }
 
     private bool ShouldEndAttackPhase(int currentBar)
@@ -323,12 +324,15 @@ public class EnemyRhythmManager : MonoBehaviour, IEasyListener
     {
         hasStartedRhythmSequence = true;
 
-        if (wave == -2 || wave == -1)
+        if (IsArrowTutorial())
             isProjectilePhase = false;
 
         rhythmSequence = GenerateRhythmBasedOnDifficulty();
         rhythmSequenceIdx = 0;
         lastSequencePlayedBar = currentBar;
+    }
+    private bool IsArrowTutorial(){
+        return wave == -1 || wave == 0;
     }
 
     private List<int> GenerateRhythmBasedOnDifficulty()
@@ -345,7 +349,7 @@ public class EnemyRhythmManager : MonoBehaviour, IEasyListener
         }
     }
 
-    private void TriggerEnemyAttack()
+    private void TriggerEnemyAttack(int currentBar, int currentBeat, int beatsPerBar)
     {
         GameObject enemy = enemies[rhythmSequenceIdx % enemies.Count];
         EnemyBehaviour enemyBehaviour = enemy.GetComponent<EnemyBehaviour>();
@@ -353,6 +357,9 @@ public class EnemyRhythmManager : MonoBehaviour, IEasyListener
         if (isProjectilePhase){
             enemyBehaviour.StartAttackAnim();
             if (rhythmSequenceIdx == 0 && IsBossWave()){
+                GameObject playerObject = GameObject.FindWithTag("Player");
+                PlayerMovement playerMovement = playerObject.GetComponent<PlayerMovement>();
+                playerMovement.SetBeatOffLimitLaneSpawn(currentBar*beatsPerBar + currentBeat);
                 SpawnRandomOffLimitLanes(2);
             }
         }
@@ -387,10 +394,10 @@ public class EnemyRhythmManager : MonoBehaviour, IEasyListener
         {
             isProjectilePhase = true;
         }
-        else if (wave == 0)
-        {
-            wave = 1;
-        }
+        // else if (wave == 0)
+        // {
+        //     wave = 1;
+        // }
 
         if (IsBossWave())
         {
@@ -418,17 +425,23 @@ public class EnemyRhythmManager : MonoBehaviour, IEasyListener
     }
 
     private bool isProjectileTutorial(int currentBar){
-        int tutorialEndBar = 15;
-        int projTutorialWave = 0;
+        
+        int projTutorialWave = -2;
+
+        if (waveStartBar == 0){
+            waveStartBar = currentBar;
+        }
+        int tutorialEndBar = waveStartBar+4;
+        Debug.Log($"currentbar {currentBar} tutorialEndBar {tutorialEndBar}");
         return wave == projTutorialWave && currentBar <= tutorialEndBar;
     }
 
     private void SpawnRandomOffLimitLanes(int numSpawnLanes){
         List<int> laneNumbers = Enumerable.Range(0, Stage.Lanes.GetNumLanes()).ToList();
 
-        GameObject playerObject = GameObject.FindWithTag("Player");
-        PlayerMovement playerMovement = playerObject.GetComponent<PlayerMovement>();
-        laneNumbers.Remove(playerMovement.currentLaneIndex); // don't spawn on player
+        // GameObject playerObject = GameObject.FindWithTag("Player");
+        // PlayerMovement playerMovement = playerObject.GetComponent<PlayerMovement>();
+        // laneNumbers.Remove(playerMovement.currentLaneIndex); // don't spawn on player
 
         for (int i = 0; i < numSpawnLanes; i++){
             int randomIndex = Random.Range(0, laneNumbers.Count);
