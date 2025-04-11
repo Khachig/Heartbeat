88be28ea8e0b8bd68c776a75f00f2ebd62014cb1
Ana Djianto <trashythingsandstuff@gmail.com>
2025-04-10 21:09:31 -0400

    add new healthbar

--- UnityProject/Assets/Scripts/Player/HealthSystem.cs
+++ UnityProject/Assets/Scripts/Player/HealthSystem.cs
@@ -55,6 +55,8 @@ public class HealthSystem : MonoBehaviour
         float healthPercentage = currentHealth / maxHealth;
         // Update the health bar
         HealthBar.value = healthPercentage;
+        
+        UpdateHealthBarGraphics(healthPercentage);
 
         // change the color of the health bar based on the health percentage
         if (healthPercentage > 0.5f)
@@ -70,6 +72,32 @@ public class HealthSystem : MonoBehaviour
         else
         {
             HealthBar.fillRect.GetComponent<UnityEngine.UI.Image>().color = Color.red;
+        }
+    }
+
+    private void UpdateHealthBarGraphics(float healthPercentage)
+    {
+        HealthBar.transform.GetChild(0).gameObject.SetActive(false); // Face for full health
+        HealthBar.transform.GetChild(1).gameObject.SetActive(false); // Face for mid health
+        HealthBar.transform.GetChild(2).gameObject.SetActive(false); // Face for low health
+        HealthBar.transform.GetChild(4).gameObject.SetActive(false); // Heart for full health
+        HealthBar.transform.GetChild(5).gameObject.SetActive(false); // Heart for mid health
+        HealthBar.transform.GetChild(6).gameObject.SetActive(false); // Heart for low health
+
+        if (healthPercentage > 0.5f)
+        {
+            HealthBar.transform.GetChild(0).gameObject.SetActive(true); // Face for full health
+            HealthBar.transform.GetChild(4).gameObject.SetActive(true); // Heart for full health
+        }
+        else if (healthPercentage > 0.25f)
+        {
+            HealthBar.transform.GetChild(1).gameObject.SetActive(true); // Face for mid health
+            HealthBar.transform.GetChild(5).gameObject.SetActive(true); // Heart for mid health
+        }
+        else
+        {
+            HealthBar.transform.GetChild(2).gameObject.SetActive(true); // Face for low health
+            HealthBar.transform.GetChild(6).gameObject.SetActive(true); // Heart for low health
         }
     }
 
